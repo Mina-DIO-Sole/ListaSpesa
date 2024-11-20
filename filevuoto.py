cd63349aaec0698713464c7b95bf20c59bdb57c1
Repository-Dio-sole <minasoliman68@gmail.com>
2024-11-20 10:57:36 +0100

    aggiunto menu e sistemato le funzioni

--- filevuoto.py
+++ filevuoto.py
@@ -7,11 +7,53 @@ lista_vuota = []
 
 
 #2
-lista_vuota.append("mele")
+def aggiungi():
+    elemento = input("Inserisci l'elemento da aggiungere alla lista: ")
+    lista_vuota.append(elemento)
+    print(f'"{elemento}" braavoo ricky aggiunto.')
 
 
 #3
-print(lista_vuota)
+def visualizza():
+ for i in range(len(lista_vuota)):
+  print(f"{i + 1}. {lista_vuota[i]}")
 
 #4
-lista_vuota.remove("mele")
+def  rimuovi_elemento_easy():
+lista_vuota.remove("mela")
+
+
+def contaElementi():
+
+
+
+def svuota_lista():
+   
+
+
+def menu():
+    while True:
+        print("\nMenu:")
+        print("1. Aggiungi un elemento alla lista")
+        print("2. Visualizza la lista")
+        print("3. Rimuovi un elemento dalla lista ")
+        print("4. conta elemetni lista ")
+        print("5. Esci")
+
+        scelta = input("Seleziona un'opzione (1-4): ")
+
+        if scelta == "1":
+            aggiungi()
+        elif scelta == "2":
+            visualizza()
+        elif scelta == "3":
+            rimuovi_elemento_easy()
+        elif scelta  == "4":
+           contaElementi()
+        elif scelta == "5":
+            print("bravo esci")
+            break
+        elif scelta == "6":
+            svuota_lista()
+        else:
+            print("Scelta no Riprova.")
