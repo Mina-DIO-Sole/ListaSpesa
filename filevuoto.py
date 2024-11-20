a5e434142919ba2e9ee3d3fa622178bbef5f4751
Repository-Dio-sole <minasoliman68@gmail.com>
2024-11-20 10:27:12 +0100

    aggiunto un metodo che aggiuge elemento alla lista

--- filevuoto.py
+++ filevuoto.py
@@ -2,5 +2,10 @@
 #programma deve permettere di aggiungere, visualizzare e
 #rimuovere elementi dalla lista.
 
-
+#1
 lista_vuota = []
+
+
+#2
+lista_vuota.append("mele")
+print("dopo append:", lista_vuota)
